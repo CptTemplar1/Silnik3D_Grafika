5b96ea7456539b68b96e2e705b62dd1bcf779b1a
CptTemplar1 <michal2001k@op.pl>
2022-12-21 15:05:38 +0100

    Poprawiona konfiguracja

--- Silnik3D_Grafika/Silnik3D_Grafika.cpp
+++ Silnik3D_Grafika/Silnik3D_Grafika.cpp
@@ -2,9 +2,31 @@
 #include <gl/freeglut.h>
 #include <iostream>
 
-#pragma comment(lib, "freeglut.lib")
+/*	OpenGL - example program with FreeGLUT / GLUT
+ *	(C) 2009, 2020 Grzegorz �ukawski, Politechnika �wi�tokrzyska w Kielcach
+ */
 
-int main()
-{
-    std::cout << "Hello World!\n";
+ // Drawing function:
+void OnDraw() {
+    // Background colour (R, G, B, Alpha):
+    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    // Clearing the colour buffer:
+    glClear(GL_COLOR_BUFFER_BIT);
+    // Flushing the rendering queue:
+    glFinish();	// or glFlush();
+}
+
+int main(int argc, char* argv[]) {
+    glutInit(&argc, argv);
+
+    // Preparing the window:
+    glutInitDisplayMode(GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Hello OpenGL");
+
+    // Drawing callback function:
+    glutDisplayFunc(OnDraw);
+
+    glutMainLoop();
+    return(0);
 }
