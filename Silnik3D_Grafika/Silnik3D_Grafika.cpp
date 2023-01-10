c228bd8a42adbbf6c0b923db84f8f9f1f52a8f35
CptTemplar1 <michal2001k@op.pl>
2023-01-10 18:47:54 +0100

    Ustawienia ścieżek

--- Silnik3D_Grafika/Silnik3D_Grafika.cpp
+++ Silnik3D_Grafika/Silnik3D_Grafika.cpp
@@ -1,12 +1,32 @@
 ﻿#include <gl/glut.h>
 #include <gl/freeglut.h>
 #include <iostream>
-#include "Engine.h"
+
+#include "../glm/gtc/type_ptr.hpp"
+#include "../glm/gtx/transform.hpp"
+
+
+ // Drawing function:
+void OnDraw() {
+    // Background colour (R, G, B, Alpha):
+    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    // Clearing the colour buffer:
+    glClear(GL_COLOR_BUFFER_BIT);
+    // Flushing the rendering queue:
+    glFinish();	// or glFlush();
+}
 
 int main(int argc, char* argv[]) {
-	glutInit(&argc, argv);
-	Engine* engine = new Engine(900, 900);
-	engine->setBackGroundColor({ 0, 0, 0, 1 });
-	engine->mainLoop();
+    glutInit(&argc, argv);
+
+    // Preparing the window:
+    glutInitDisplayMode(GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Hello OpenGL");
+
+    // Drawing callback function:
+    glutDisplayFunc(OnDraw);
+
+    glutMainLoop();
     return(0);
 }
